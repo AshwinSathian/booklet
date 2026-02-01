cbd355db98fae7ea4151909d98e4fc0563f161d0
AshwinSathian <ashwinsathyan19@gmail.com>
2026-02-01 23:24:54 +0530

    Fix build errors

--- src/app/apple-icon.tsx
+++ src/app/apple-icon.tsx
@@ -1,1 +1,8 @@
-export { contentType, default, runtime, size } from "./icon";
+export { default } from "./icon";
+
+export const runtime = "edge";
+export const size = {
+  width: 512,
+  height: 512,
+};
+export const contentType = "image/png";
